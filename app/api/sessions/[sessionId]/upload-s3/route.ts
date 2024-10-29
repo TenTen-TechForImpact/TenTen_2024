7caacaee18640a21771cbb8e404061f3d3b90c1f
Lily Yerim Kim <77142800+lilble@users.noreply.github.com>
2024-10-29 19:17:11 +0900

    upload recording

--- app/api/sessions/[sessionId]/upload-s3/route.ts
+++ app/api/sessions/[sessionId]/upload-s3/route.ts
@@ -7,8 +7,8 @@ const supabase = createClient();
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
