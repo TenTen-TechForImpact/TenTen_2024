7d8086d94bde6de29addb85e459813bda6709c1e
이정호 <hohoho4u4me@gmail.com>
2024-11-05 00:40:28 +0900

    환경변수 변경 취소

--- utils/supabase/component.ts
+++ utils/supabase/component.ts
@@ -2,8 +2,8 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   const supabase = createBrowserClient(
-    process.env.REACT_APP_NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.REACT_APP_NEXT_PUBLIC_SUPABASE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!
   );
 
   return supabase;
