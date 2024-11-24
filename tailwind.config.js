bceea92ff11624a8200dba26cb047f3f6a994b61
이정호 <hohoho4u4me@gmail.com>
2024-11-24 18:16:02 +0900

    UI : 사이드바 개발 중

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,31 +9,7 @@ module.exports = {
     flowbite.content(),
   ],
   theme: {
-    extend: {
-      colors: {
-        white: "#FBFBFE",
-        black: "#050314",
-        primary: "#2B2AC9",
-        secondary: "#DEDCFD",
-        accent: "#AEDAFD",
-      },
-      fontSize: {
-        title: "48px",
-        "extra-large": "32px",
-        large: "24px",
-        medium: "16px",
-        small: "12px",
-      },
-      spacing: {
-        small: "8px",
-        medium: "20px",
-        page: "55px",
-      },
-      borderRadius: {
-        medium: "20px",
-        large: "30px",
-      },
-    },
+    extend: {},
   },
   plugins: [flowbite.plugin()],
 };
